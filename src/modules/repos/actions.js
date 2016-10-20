be0b22aaee567a26c390f8c208c61f534ec0f3b0
Andrei Cacio <andrei.cacio@yahoo.com>
2016-10-20 16:53:45 +0300

    Repository list now works

--- src/modules/repos/actions.js
+++ src/modules/repos/actions.js
@@ -4,7 +4,7 @@ import { RECEIVE_REPOS } from './action-types';
 const fetchedRepos = (repos) => ({ type: RECEIVE_REPOS, repos });
 
 export const fetchRepos = () => (dispatch, getState) => {
-  const { username } = getState().userInformation.info;
+  const username = getState().userInformation.info.login;
   fetch(API.GITHUB.repos(username))
     .then(res => res.json())
     .then(res => dispatch(fetchedRepos(res)));
